9e53a779a5fed933ebb5047b9dc278e26c2922fe
keyuquan <ke15391609819>
2021-06-17 13:47:53 +0800

    httpUtils删除get body 请求方式

--- src/main/java/com/collect/boom/controller/TrackingIoGetController.java
+++ src/main/java/com/collect/boom/controller/TrackingIoGetController.java
@@ -1,10 +1,12 @@
 package com.collect.boom.controller;
 
 import com.alibaba.fastjson.JSONObject;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.HashMap;
-import java.util.Map;
 
 /**
  * 热云数据同步
